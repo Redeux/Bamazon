91052abb88ce8a8259acc645092247bd2cc848c8
Phil Sautter <psautter@gmail.com>
2017-05-04 22:21:57 -0400

    bamazonCustomer working

---
+++ DBInstance.js
@@ -0,0 +1,53 @@
+const dbConfig = require("./db.json"),
+    mysql = require("mysql");
+
+function DBInstance() {
+    this.connection = mysql.createConnection(dbConfig);
+}
+
+DBInstance.prototype.connect = function() {
+    this.connection.connect(error => {
+        if (error) throw error
+    });
+}
+
+DBInstance.prototype.end = function() {
+    this.connection.end();
+}
+
+DBInstance.prototype.select = function(select, from, callback) {
+    this.connection.query('SELECT ?? FROM ??', [select, from], (error, response) => {
+        if (error) return callback(error, null);
+        else return callback(null, response);
+    });
+}
+
+DBInstance.prototype.selectWhere = function(select, from, where, callback) {
+    this.connection.query('SELECT ?? FROM ?? WHERE ?', [select, from, where], (error, response) => {
+        if (error) return callback(error, null);
+        else return callback(null, response);
+    });
+}
+
+DBInstance.prototype.insert = function(into, set, callback) {
+    this.connection.query('INSERT INTO ?? SET ?', [into, set], (error, response) => {
+        if (error) return callback(error, null);
+        else return callback(null, response);
+    });
+}
+
+DBInstance.prototype.updateWhere = function(update, set, where, callback) {
+    this.connection.query('UPDATE ?? SET ? WHERE ?', [update, set, where], (error, response) => {
+        if (error) return callback(error, null);
+        else return callback(null, response);
+    });
+}
+
+DBInstance.prototype.deleteWhere = function(from, where, callback) {
+    this.connection.query('DELETE FROM ?? WHERE ?', [from, where], (error, response) => {
+        if (error) return callback(error, null);
+        else return callback(null, response);
+    });
+}
+
+module.exports = DBInstance;
--- bamazon.sql
+++ bamazon.sql
@@ -5,7 +5,7 @@ CREATE TABLE products (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
-    price INT NOT NULL,
+    price DECIMAL(6,2) NOT NULL,
     stock_quantity INT NOT NULL
 );
 
